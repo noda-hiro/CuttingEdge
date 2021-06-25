09464eac445080f7d0edaf518ec3d727408af453
5bou <emptyytpme117711@gmail.com>
2021-06-25 21:09:05 +0900

    6/25までに作成したもの ・エネミー・飛翔の行動パターンの完成(細かい挙動の修正はまた後で)

---
+++ Assets_Enemy/Scripts/Controls/Aerial_Slash_Control.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aerial_Slash_Control : MonoBehaviour
+{
+	// 真空波の速度
+	[SerializeField]
+	float aerial_slashSpeed = 0.0f;
+
+	// 別のスクリプトにある値を入れておく変数
+	int PlayerHP = 0;
+	int EnemyATK = 0;
+
+	// 出現から消滅のタイマー
+	float disappearTimer = 4.0f;
+
+	// プレイヤーの座標を取得したかどうかのフラグ
+	bool player_coordinateFlag = false;
+
+	// GameObjectを入れておくやつ
+	private GameObject PlayerObject;
+	private GameObject EnemyObject;
+
+	// 座標を入れておくやつ
+	private Vector3 player_position;
+
+	// スクリプトを入れておくやつ
+	private Test_Player_Control PlayerScript;
+	private Enemy_Flying_Control FlyingScript;
+
+	/*----------------------------------------------------------------------------------------------------*/
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		// プレイヤーのオブジェクトを取得
+		PlayerObject = GameObject.Find("Test_Player");
+		// プレイヤーのスクリプトを取得
+		PlayerScript = PlayerObject.GetComponent<Test_Player_Control>();
+		// プレイヤーのHPを取得
+		PlayerHP = PlayerScript.hp;
+
+		// エネミー・飛翔のオブジェクトを取得
+		EnemyObject = GameObject.Find("Enemy_Flying(Clone)");
+		// エネミー・飛翔のスクリプトを取得
+		FlyingScript = EnemyObject.GetComponent<Enemy_Flying_Control>();
+		// エネミー・飛翔のATKを取得
+		EnemyATK = FlyingScript.atk;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// 1秒ずつ減らす
+		disappearTimer -= Time.deltaTime;
+
+		// プレイヤーの座標を1回だけ取得する
+		PLayerPosition();
+
+		// 取得したプレイヤーの座標に向かって移動する
+		AerialSlash_Move();
+
+		// 4秒経過したら、自分を破壊する
+		TimeOver();
+	}
+
+	// プレイヤーに真空波が当たったとき
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		// プレイヤーのHPをエネミー・飛翔のATK分だけ減らす
+		PlayerHP -= EnemyATK;
+		PlayerScript.hp = PlayerHP;
+
+		// 消滅する
+		Disappear();
+	}
+
+	// 自身を破壊する関数
+	private void Disappear()
+	{
+		Destroy(this.gameObject);
+	}
+
+	// プレイヤーの座標を1回だけ取得する関数
+	private void PLayerPosition()
+	{
+		// player_coordinateFlagがfalseのとき実行
+		if (player_coordinateFlag == false)
+		{
+			// プレイヤーの座標を取得
+			player_position = PlayerObject.transform.position;
+
+			// player_coordinateFlagをtrueにする
+			player_coordinateFlag = true;
+		}
+	}
+
+	// プレイヤーが居た座標まで、一定の速度で進む関数
+	private void AerialSlash_Move()
+	{
+		float move = aerial_slashSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, player_position*2, move);
+	}
+
+	// 4秒経過したら、自信を破壊する関数
+	private void TimeOver()
+	{
+		// disappeatTimerが0秒以下になったら実行
+		if(disappearTimer <= 0)
+		{
+			Disappear();
+		}
+	}
+}
--- Assets_Enemy/Scripts/Controls/Enemy_Flying_Control.cs
+++ Assets_Enemy/Scripts/Controls/Enemy_Flying_Control.cs
@@ -10,18 +10,25 @@ public class Enemy_Flying_Control : MonoBehaviour
 	// エネミー・飛翔の攻撃力
 	[SerializeField]
 	int ATK = 8;
-	[SerializeField]
 	// エネミー・飛翔の移動速度
+	[SerializeField]
 	float SPEED = 0.0f;
+	// エネミー・飛翔の突進速度
+	[SerializeField]
+	float RushSpeed = 0.0f;
 
-	// おぎゃりたてかどうか
-	bool spawnFlag = true;
+	// ATKのプロパティー
+	public int atk
+	{
+		get { return ATK; }
+	}
 
-	// IEnumeratorで停止する秒数
-	float StopTime = 0.5f;
-
-	// EM_forPlayerでプレイヤーに向かって移動するときに必要なやつ
+	// GameObjectを入れておくやつ
 	private GameObject PlayerObject;
+
+	// 座標を入れておくやつ
+	private Vector3 BackPosition;
+	private Vector3 PlayerPosition;
 
 	// 可視状態かどうか
 	private bool isVisible = false;
@@ -31,26 +38,44 @@ public class Enemy_Flying_Control : MonoBehaviour
 		get { return isVisible; }
 	}
 
+	// おぎゃりたてかどうか
+	bool spawnFlag = true;
 	// Babuが終わったかどうかのフラグ
 	bool babu_endFlag = false;
-	// Flying_Actのifが実行中かどうかのフラグ
+	// Flying_Act_Flagのifが実行中かどうかのフラグ
 	bool duringFlag = false;
+	// 突進攻撃のときにY座標の取得をしたかどうかのフラグ
+	bool coordinate_getFlag = false;
+	// 真空波攻撃をもう放ったかどうかのフラグ
+	bool fireFlag = false;
+
 	// Flying_Act -> 移動用のフラグ
 	bool moveFlag = false;
 	// Flying_Act -> 待機用のフラグ
 	bool idleFlag = false;
+	// Flying_Act -> 突進攻撃用のフラグ
+	bool rushFlag = false;
+	// Flying_Act -> 真空波攻撃用のフラグ
+	bool aerial_slashFlag = false;
 
 	// 移動用のタイマー
 	float moveTimer = 4.0f;
 	// 待機用のタイマー
 	float idleTimer = 3.0f;
+	// 突進攻撃用のタイマー
+	float rushTimer = 4.0f;
+	// 真空波攻撃用のタイマー
+	float aerial_slashTimer = 3.0f;
+
+	// IEnumeratorで停止する秒数
+	float StopTime = 0.5f;
 
 	/*----------------------------------------------------------------------------------------------------*/
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		// プレイヤーのオブジェクトの座標を取得
+		// プレイヤーのオブジェクトを取得
 		PlayerObject = GameObject.Find("Test_Player");
 	}
 
@@ -66,23 +91,25 @@ public class Enemy_Flying_Control : MonoBehaviour
 		// おぎゃおぎゃしたので実行！
 		Babu();
 
+		// 各種行動のフラグ行動管理
 		if (babu_endFlag == true)
 		{
-			Flying_Act();
+			Flying_Act_Flag();
 		}
 
+		// MOVE
 		// 4秒間、プレイヤーに向かって移動をする
 		if (moveFlag == true)
 		{
 			Debug.Log("MOVE");
 
-			// プレイヤーに向かって移動
-			EM_forPlayer();
-
 			// moveTimerを1秒ずつ減らす
 			moveTimer -= 1.0f * Time.deltaTime;
 
-			// moveTimerが0秒以下なら、実行
+			// プレイヤーに向かって移動
+			EM_forPlayer();
+
+			// moveTimerが0秒以下なら実行
 			if (moveTimer <= 0)
 			{
 				// moveFlagをfalseにする
@@ -90,9 +117,9 @@ public class Enemy_Flying_Control : MonoBehaviour
 				// duringFlagをfalseにする
 				duringFlag = false;
 			}
-
 		}
 
+		// IDLE
 		// 3秒間、その場で待機
 		if (idleFlag == true)
 		{
@@ -101,11 +128,106 @@ public class Enemy_Flying_Control : MonoBehaviour
 			// idleTimerを1秒ずつ減らす
 			idleTimer -= 1.0f * Time.deltaTime;
 
-			// idleTimerが0秒以下なら、実行
+			// idleTimerが0秒以下なら実行
 			if (idleTimer <= 0)
 			{
 				// idleFlagをfalseにする
 				idleFlag = false;
+				// duringFlagをfalseにする
+				duringFlag = false;
+			}
+		}
+
+		// RUSH_ATTACK
+		// 2秒かけてプレイヤーの座標まで移動、後に2秒かけて元のyに戻る
+		if(rushFlag == true)
+		{
+			Debug.Log("RUSH");
+
+			// rushTimerを1秒ずつ減らす
+			rushTimer -= 1.0f * Time.deltaTime;
+
+			// coordinate_getFlagがfalseなら実行
+			if (coordinate_getFlag == false)
+			{
+				// 自身の座標を取得
+				BackPosition = transform.position;
+				// プレイヤーの座標を取得
+				PlayerPosition = PlayerObject.transform.position;
+				// 取得したのでtrueにする
+				coordinate_getFlag = true;
+			}
+
+			// rushTimerが2秒以上、3秒未満なら実行
+			if (2.0 <= rushTimer && rushTimer < 3.0)
+			{
+				// プレイヤーに向かって突進
+				Attack_Rush();
+			}
+			// rushTimerが0秒より上、2.0秒未満なら実行
+			else if(0.0 < rushTimer && rushTimer < 2.0)
+			{
+				// 戻る
+				Rush_Back();
+			}
+			//rushTimerが0秒以下なら実行
+			else if (rushTimer <= 0)
+			{
+				// coordinate_getFlagをfalseにする
+				coordinate_getFlag = false;
+				// rushFlagをfalseにする
+				rushFlag = false;
+				// duringFlagをfalseにする
+				duringFlag = false;
+			}
+		}
+
+		// AERIAL-SLASH_ATTACK
+		// ゆっくり気味にプレイヤーへ向かって飛ぶ
+		if(aerial_slashFlag == true)
+		{
+			Debug.Log("AERIAL-SLASH");
+
+			// aerial_slashTimerを1秒ずつ減らす
+			aerial_slashTimer -= 1.0f * Time.deltaTime;
+
+			// aerial_slashTimerが2秒以上、3秒未満なら実行
+			if (2.0 <= aerial_slashTimer && aerial_slashTimer < 3.0)
+			{
+				// Debug.Log("攻撃準備");
+				// 攻撃準備
+			}
+			// aerial_slashTimerが0秒より上、2秒未満なら実行
+			else if (0.0 < aerial_slashTimer && aerial_slashTimer < 2.0)
+			{
+				// Debug.Log("攻撃");
+
+				// すでに真空波を放っていたら実行
+				if(fireFlag == true)
+				{
+					// Debug.Log("発射済みだよ～ん");
+				}
+				// まだ真空波を放っていなかったら実行
+				else if(fireFlag == false)
+				{
+					// Debug.Log("発射！");
+
+					// 真空波プレハブをGameObject型で取得
+					GameObject aerialSlash = (GameObject)Resources.Load("Prefabs/Flying_AerialSlash");
+					// 真空波プレハブを元に、インスタンスを生成
+					Instantiate(aerialSlash, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+
+					// fireFlagをtrueにする
+					fireFlag = true;
+				}
+			}
+			// aerial_slashTimerが0秒以下なら、実行
+			else if (aerial_slashTimer <= 0)
+			{
+				// fireFlagをfalseにする
+				fireFlag = false;
+				// aerial_slashFlagをfalseにする
+				aerial_slashFlag = false;
 				// duringFlagをfalseにする
 				duringFlag = false;
 			}
@@ -128,6 +250,20 @@ public class Enemy_Flying_Control : MonoBehaviour
 	{
 		float move = SPEED * Time.deltaTime;
 		transform.position = Vector3.MoveTowards(transform.position, PlayerObject.transform.position, move);
+	}
+
+	// 突進用関数
+	private void Attack_Rush()
+	{
+		float rush = RushSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, PlayerPosition*2, rush);
+	}
+
+	// 突進後に戻る用関数
+	private void Rush_Back()
+	{
+		float back = SPEED * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, BackPosition, back);
 	}
 
 	// スポーンしたてのときに実行する関数
@@ -158,7 +294,7 @@ public class Enemy_Flying_Control : MonoBehaviour
 	}
 
 	// エネミーの行動関数
-	private void Flying_Act()
+	private void Flying_Act_Flag()
 	{
 		// Randomクラスのインスタンスを生成
 		var act_rand_instance = new System.Random();
@@ -177,6 +313,7 @@ public class Enemy_Flying_Control : MonoBehaviour
 				moveFlag = true;
 				duringFlag = true;
 
+				// Timerを元に戻す
 				moveTimer = 4.0f;
 			}
 
@@ -191,22 +328,45 @@ public class Enemy_Flying_Control : MonoBehaviour
 				idleFlag = true;
 				duringFlag = true;
 
+				// Timerを元に戻す
 				idleTimer = 3.0f;
 			}
 
 			// Debug.Log("stay");
 		}
 		else if (flying_act == 2)       // 突進攻撃
-		{
+		{          
+			// duringFlag がfalseなら実行
+			if (duringFlag == false)
+			{
+				// rushFlagとduringFlagをtrueにする
+				rushFlag = true;
+				duringFlag = true;
+
+				// Timerを元に戻す
+				rushTimer = 4.0f;
+			}
+
 			// Debug.Log("attack");
 		}
 		else if (flying_act == 3)       // 真空波攻撃
-		{
+		{           
+			// duringFlag がfalseなら実行
+			if (duringFlag == false)
+			{
+				// aerial_slashFlagとduringFlagをtrueにする
+				aerial_slashFlag = true;
+				duringFlag = true;
+
+				// Timerを元に戻す
+				aerial_slashTimer = 3.0f;
+			}
+
 			// Debug.Log("wave_attack");
 		}
 	}
 
-	// Babu専用のオブジェクト停止関数
+	// Babu専用のオブジェクト停止コルーチン
 	IEnumerator Stop()
 	{
 		 EM_forPlayer();
@@ -217,14 +377,14 @@ public class Enemy_Flying_Control : MonoBehaviour
 		// おぎゃりたてでなくなる
 		spawnFlag = false;
 
-		Debug.Log("おぎゃりを不可能状態");
+		// Debug.Log("おぎゃりを不可能状態");
 
 		// 2秒待つ
 		yield return new WaitForSeconds(2);
 
 		babu_endFlag = true;
 
-		Debug.Log("babuが終わりました");
+		// Debug.Log("babuが終わりました");
 		// Debug.Log(spawnFlag);
 	}
 }
